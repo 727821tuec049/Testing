c309082ef3a47d86af86fd15e52301b7b790295e
727821tuec049 <115608960+727821tuec049@users.noreply.github.com>
2023-06-07 09:59:42 +0530

    Add files via upload

---
+++ day5t/Saturday/src/main/java/AutoTest/Saturday/App.java
@@ -0,0 +1,13 @@
+package AutoTest.Saturday;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
