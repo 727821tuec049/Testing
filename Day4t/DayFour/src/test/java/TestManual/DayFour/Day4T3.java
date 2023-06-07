fe0cf25e17afede52ca91d085ea7de5deb4a0420
727821tuec049 <115608960+727821tuec049@users.noreply.github.com>
2023-06-07 09:56:27 +0530

    Add files via upload

---
+++ Day4t/DayFour/src/test/java/TestManual/DayFour/Day4T3.java
@@ -0,0 +1,29 @@
+package TestManual.DayFour;
+
+
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Day4T3 {
+
+
+
+	public static void main(String args[])
+	{
+		WebDriverManager.chromedriver().setup();
+		WebDriver wd = new ChromeDriver();
+		wd.get("https://demo.opencart.com/");
+		wd.manage().window().maximize();
+		wd.findElement(By.xpath("//*[@id=\'content\']/h3"));
+	}
+
+
+
+
+
+
+}
